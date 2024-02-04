3b98d17ff373d2e56fb06a6896124ba07eb545be
Davron-Adbusalomov <115224119+Davron-Adbusalomov@users.noreply.github.com>
2024-02-04 15:59:48 +0500

    Update DigitsSumCalculator.java

--- src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
+++ src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
@@ -2,5 +2,14 @@ package lang.print.gaps.finalModuleTask;
 
 public class DigitsSumCalculator {
     public void calculateSum(int number) {
+         int thousands = number / 1000;
+        int hundreds = (number / 100) % 10;
+        int tens = (number / 10) % 10;
+        int units = number % 10;
+
+        // Calculate the sum of all digits
+        int sum = thousands + hundreds + tens + units;
+
+        System.out.println(sum);
     }
 }
