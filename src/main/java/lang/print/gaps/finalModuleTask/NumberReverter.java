f4682509b362d801cefdb078270a438d8c18cd4d
Davron-Adbusalomov <115224119+Davron-Adbusalomov@users.noreply.github.com>
2024-02-04 15:57:56 +0500

    Update NumberReverter.java

--- src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
@@ -2,5 +2,12 @@ package lang.print.gaps.finalModuleTask;
 
 public class NumberReverter {
     public void revert(int number) {
+        int num1=number%10;
+        int num2 = (number-num1)%100;
+        int num3 = number/100;
+        int result = num1*100+num2*10+num3;
+
+        System.out.println(result);
+        
     }
 }
